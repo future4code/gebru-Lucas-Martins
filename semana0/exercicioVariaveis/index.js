d8ec8c7ec06e56a39ad46138143b50a12dc14da3
HexMagnus <luke.dev.fullstack@outlook.com>
2021-11-09 21:59:41 -0300

    Atividade Finalizada
    Atividade Finalizada

--- semana0/exercicioVariaveis/index.js
+++ semana0/exercicioVariaveis/index.js
@@ -1,10 +1,90 @@
+//EXERCICIOS JS - Lucas Martins - Gebru
 
+//1)Construa um programa, seguindo os seguintes passos:
 
+/*a) Declare uma variável para armazenar um nome, sem atribuir um valor.
 
-let a = 10
-let b = 20
-c = a
-b = c
-a = b
+b) Declare uma variável para armazenar uma idade, sem atribuir um valor.
 
-console.log(a, b, c)
+c) Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando `typeof`.
+
+d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+
+e) Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+
+f) Novamente, imprima na tela o tipo dessas variáveis. O que você notou? Escreva em um comentário de código.
+
+g) Para finalizar, imprima na tela a mensagem: "Olá nome,  você tem idade anos". Onde nome e idade são os valores que o usuário inseriu
+
+-----Done-------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+              //>>>........................Respostas..................<<//
+//a)
+//var nome
+//b)
+//var idade
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+//c) undefined
+
+//d)Como o próprio nome diz, não foi definido o "type" tipo.
+
+//e)
+//const nome = prompt('Qual o seu nome completo?')
+//const idade = prompt ("Qual a sua idade?")
+
+//console.log("Meu nome completo é " , nome," e tenho ", idade, "anos")
+
+//f) Ele buscou coletar informarções do prompt para preencher a linha de impressão.
+
+//g)
+//const nome = prompt('Qual o seu nome completo?')
+//const idade = prompt ("Qual a sua idade?")
+
+//console.log("Seu nome é " , nome," e você tem ", idade, "anos")
+/*
+---Done------------------------------------------------------------------------------------------------------
+*/
+
+/*2) Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+    
+    a) Crie três novas variáveis, contendo as respostas
+    
+    b) Imprima na tela todas as perguntas seguidas por suas respectivas respostas. Por exemplo:
+        
+    Você está usando uma roupa azul hoje? - SIM
+
+    -------Done----------------------------------------------------------------------------------------
+    //>>>........................Respostas..................<<//
+*/
+
+//a) 
+
+//let abacate = true
+//let carrapato = false
+//let labenu = false
+
+//b)
+//console.log("Você gosta de abacate?", abacate)
+//console.log("Carrapato tem mãe e pai?", carrapato )
+//console.log("Você não irá terminar o curso?", labenu)
+
+/*
+-------Done----------------------------------------------------------------------------------------
+*/
+//3)
+
+//let a = 10
+//let b = 25
+//let c 
+
+//c = a
+//a = b
+//b = c
+
+//console.log('O novo valor de a é',c)
+//console.log('O novo valor de b é',a )
+
