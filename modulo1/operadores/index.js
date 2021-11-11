ab99340e72e098cef0e969ec31de5c5b168addc1
HexMagnus <luke.dev.fullstack@outlook.com>
2021-11-11 20:17:19 -0300

    Update index.js

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -1,13 +1,13 @@
 //EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO
 
-const bool1 = true
-const bool2= false 
-const bool3= !bool2
+//const bool1 = true
+//const bool2= false 
+//const bool3= !bool2
 
-let resultado  = bool1 && bool2
+//let resultado  = bool1 && bool2
 //bool1 = true +bool2 = true
-console.log("a. ", resultado)
-//true+true = false
+//console.log("a. ", resultado)
+//true+false+false = false
 //a)false
 //---------------------------------------------------------------------------------------------------------------------//
 //resultado = bool1 && bool2 && bool3
@@ -23,3 +23,35 @@ console.log("a. ", resultado)
 //c)true
 //console.log("d. ", typeof resultado)
 //booleanos
+
+//---------------------------------------------------------------------------------------------------------------------//
+
+//2)
+//let primeiroNumero = prompt("Digite um numero!")
+//let segundoNumero = prompt("Digite outro numero!")
+
+//const soma = primeiroNumero + segundoNumero
+
+//console.log(soma)
+
+//---------------------------------------------------------------------------------------------------------------------//
+
+//3)
+
+//let primeiroNumero = Number (prompt("Digite um numero!"))
+//let segundoNumero = Number (prompt("Digite outro numero!"))
+//const soma = primeiroNumero + segundoNumero
+
+//console.log(soma)
+
+//EXERCICIOS DE ESCRITA DE CÓDIGO
+
+//1)
+//a)
+let idadeUm = Number(prompt("Qual é a sua idade?"))
+//b)
+let idadeDois=Number(prompt("Qual é a idade do seu melhor amig?"))
+let idadeUm
+ console.log("Sua idade é maior do que a do seu melhor amig?")
+
+
